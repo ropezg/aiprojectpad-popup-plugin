a2ffda55a490b8be480e7f1371294fd713737b43
ropezg <rope.zg@gmail.com>
2024-02-29 16:17:28 +0100

    Update settings-page.php

--- admin/settings-page.php
+++ admin/settings-page.php
@@ -10,10 +10,10 @@ if (!current_user_can('manage_options')) {
     <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
     <form action="options.php" method="post">
         <?php
-        // Output security fields for the registered setting "aipowerpad_popup_option_group"
-        settings_fields('aipowerpad_popup_option_group');
+        // Output security fields for the registered setting "aiprojectpad_popup_option_group"
+        settings_fields('aiprojectpad_popup_option_group');
         // Output setting sections and their fields
-        do_settings_sections('aipowerpad-popup-admin');
+        do_settings_sections('aiprojectpad-popup-admin');
         // Output save settings button
         submit_button('Save Settings');
         ?>
